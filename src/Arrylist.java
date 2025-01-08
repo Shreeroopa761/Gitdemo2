ca7c09090766a8f816c65623d43725ba8c81638a
roopashree <shreeroopa761@gmail.com>
2025-01-08 13:27:50 +0530

    Initial commit

---
+++ src/Arrylist.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class Arrylist {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String[] name1 = {"John","Jack","Jill"};
+		List<String> nameArrayList = Arrays.asList(name1);
+		nameArrayList.contains("Jack");
+	}
+
+}
